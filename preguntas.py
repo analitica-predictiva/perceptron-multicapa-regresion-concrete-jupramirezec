69aee14140bcb334c4416d05bf28089e18f5f6ac
jupramirezec <36797104+jupramirezec@users.noreply.github.com>
2023-11-29 01:48:43 -0500

    Segunda revision

--- preguntas.py
+++ preguntas.py
@@ -150,20 +150,20 @@ def pregunta_05():
     estimator = pregunta_04()
 
     # Entrene el estimador
-    estimator.fit(x_train, y_train)  #
+    estimator.fit(x_train, y_train)  
 
     # Pronostique para las muestras de entrenamiento y validacion
-    y_trian_pred = estimator.best_estimator_.predict(x_train)  
-    y_test_pred = estimator.best_estimator_.predict(x_test) 
+    y_train_pred = estimator.predict(x_train)  
+    y_test_pred = estimator.predict(x_test) 
 
     # Calcule el error cuadrático medio de las muestras
     mse_train = mean_squared_error(  
         y_train,  
-        y_trian_pred,  
+        y_train_pred  
     )
     mse_test = mean_squared_error(  
         y_test,  
-        y_test_pred,  
+        y_test_pred  
     )
 
     # Retorne el mse de entrenamiento y prueba
